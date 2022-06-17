90e45b6b522fc3dd812dcc371af45efff2f66cf2
Pin Loon Lee <pinloon_0428@hotmail.com>
2022-06-17 22:09:05 +0800

    CMakeLists.txt, cmake, conanfile.py: made repo as conan package

---
+++ conanfile.py
@@ -0,0 +1,29 @@
+from conans import ConanFile, CMake, tools
+
+class AlgorithmConan(ConanFile):
+    name = "algorithm"
+    version = "0.1.0"
+    license = "proprietary@pinloon"
+    author = "Pin Loon Lee"
+    url = "<Package recipe repository url here, for issues about the package>"
+    description = "Algorithm"
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False]}
+    default_options = {"shared": False}
+    generators = "cmake_paths"
+    exports_sources = "*"
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.definitions["ALGO_PACK"] = True
+        cmake.configure()
+        cmake.build()
+        
+    def package(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.install()
+
+    def package_info(self):
+        self.cpp_info.includedirs = ["include"]
+        self.cpp_info.libs = ["algorithm"]
