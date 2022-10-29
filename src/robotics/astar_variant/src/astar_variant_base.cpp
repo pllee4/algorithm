6a5e513c90bd904bbf2e82462ec7a27632f6c74b
Pin Loon Lee <pinloon_0428@hotmail.com>
2022-10-29 11:44:50 +0800

    src: robotics: astar_variant_base.cpp: completed implementation

---
+++ src/robotics/astar_variant/src/astar_variant_base.cpp
@@ -0,0 +1,157 @@
+/*
+ * astar_variant_base.cpp
+ *
+ * Created on: Oct 26, 2022 22:31
+ * Description:
+ *
+ * Copyright (c) 2022 Pin Loon Lee (pllee4)
+ */
+
+#include "algorithm/robotics/astar_variant/astar_variant_base.hpp"
+
+namespace pllee4::graph {
+void AstarVariantBase::SetMapStorageSize(const size_t x_size,
+                                         const size_t y_size) {
+  map_storage_ = std::make_unique<MapStorage>(x_size, y_size);
+  std::vector<std::vector<bool>> visited_map(x_size,
+                                             std::vector<bool>(y_size, false));
+  visited_map_ = visited_map;
+}
+
+bool AstarVariantBase::SetOccupiedGrid(
+    const std::vector<Coordinate> &occupied_grid) {
+  if (map_storage_) {
+    auto &map = map_storage_->GetMap();
+    for (const auto &coordinate : occupied_grid) {
+      if (map_storage_->Contains(coordinate)) {
+        map[coordinate.x][coordinate.y].occupied = true;
+      } else {
+        return false;
+      }
+    }
+    return true;
+  }
+  return false;
+}
+
+bool AstarVariantBase::SetStartAndDestination(const Coordinate &start,
+                                              const Coordinate &dest) {
+  if (map_storage_) {
+    if (map_storage_->Contains(start) && map_storage_->Contains(dest)) {
+      // new start / destination
+      if (!(start_ == start && dest_ == dest)) {
+        found_path_ = false;
+        start_ = start;
+        dest_ = dest;
+
+        // clear possible historical search
+        traverse_path_.clear();
+        map_storage_->ResetCost();
+        for (auto &row : visited_map_) {
+          std::fill(row.begin(), row.end(), false);
+        }
+
+        auto &map = map_storage_->GetMap();
+        traverse_path_.insert(
+            std::make_pair(0, std::make_pair(start.x, start.y)));
+        map[start.x][start.y].parent_coordinate = start;
+        map[start.x][start.y].f = 0;
+        map[start.x][start.y].g = 0;
+        map[start.x][start.y].h = 0;
+      }
+      start_and_end_set_ = true;
+      return true;
+    }
+  }
+  return false;
+}
+
+std::optional<std::vector<Coordinate>> AstarVariantBase::StepOverPathFinding() {
+  if (start_and_end_set_) {
+    if (!traverse_path_.empty()) {
+      const auto travelled_path = traverse_path_.begin();
+      traverse_path_.erase(traverse_path_.begin());
+
+      const auto [i, j] = travelled_path->second;
+
+      // mark node as visited
+      visited_map_[i][j] = true;
+
+      auto &map = map_storage_->GetMap();
+
+      std::vector<Coordinate> expanded_nodes;
+
+      for (size_t k = 0; k < motion_constraint_.size(); ++k) {
+        Coordinate coordinate;
+        coordinate.x = i + motion_constraint_.dx[k];
+        coordinate.y = j + motion_constraint_.dy[k];
+        if (!map_storage_->Contains(coordinate)) continue;
+
+        if (coordinate == dest_) {
+          map[coordinate.x][coordinate.y].parent_coordinate = {i, j};
+          found_path_ = true;
+          break;
+        }
+
+        // not occupied
+        if (!map[coordinate.x][coordinate.y].occupied &&
+            !visited_map_[coordinate.x][coordinate.y]) {
+          const auto dist_travelled = sqrt(abs(motion_constraint_.dx[k]) +
+                                           abs(motion_constraint_.dy[k]));
+          const auto new_g = map[i][j].g + dist_travelled;
+          const auto new_h = GetHeuristicFunc()(coordinate.x - dest_.x,
+                                                coordinate.y - dest_.y);
+          const auto new_f = new_g + new_h;
+
+          if (map[coordinate.x][coordinate.y].f ==
+                  std::numeric_limits<MapStorage::CostDataType>::max() ||
+              map[coordinate.x][coordinate.y].f >= new_f) {
+            map[coordinate.x][coordinate.y].f = new_f;
+            map[coordinate.x][coordinate.y].g = new_g;
+            map[coordinate.x][coordinate.y].h = new_h;
+            map[coordinate.x][coordinate.y].parent_coordinate = {i, j};
+
+            traverse_path_.insert(std::make_pair(
+                new_f, std::make_pair(coordinate.x, coordinate.y)));
+
+            expanded_nodes.push_back(coordinate);
+          }
+        }
+      }
+      if (found_path_) return std::nullopt;
+      return expanded_nodes;
+    }
+  }
+  return std::nullopt;
+}
+bool AstarVariantBase::FindPath() {
+  if (!start_and_end_set_) return false;
+  while (StepOverPathFinding().has_value()) {
+    StepOverPathFinding();
+  }
+  return found_path_;
+}
+
+std::optional<std::vector<Coordinate>> AstarVariantBase::GetPath() {
+  if (found_path_) {
+    auto &map = map_storage_->GetMap();
+    std::vector<Coordinate> path;
+    auto [x, y] = dest_;
+    while (!(map[x][y].parent_coordinate.x == x &&
+             map[x][y].parent_coordinate.y == y)) {
+      path.emplace_back(Coordinate{x, y});
+      const auto [temp_x, temp_y] = map[x][y].parent_coordinate;
+      std::tie(x, y) = std::pair(temp_x, temp_y);
+    }
+    path.emplace_back(Coordinate{x, y});
+    std::reverse(path.begin(), path.end());
+    return path;
+  } else {
+    return std::nullopt;
+  }
+}
+
+void AstarVariantBase::SetMotionConstraint(MotionConstraint motion_constraint) {
+  motion_constraint_ = motion_constraint;
+}
+}  // namespace pllee4::graph
