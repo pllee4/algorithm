d601080d32428f4a7a35799d8583963b05f4ca9c
Pin Loon Lee <pinloon_0428@hotmail.com>
2022-11-06 14:10:09 +0800

    src: robotics: astar_test, dijikstra_test: added test for reset

--- src/robotics/dijikstra/test/dijikstra_test.cpp
+++ src/robotics/dijikstra/test/dijikstra_test.cpp
@@ -85,6 +85,75 @@ TEST(Dijikstra, GetPath) {
                          std::end(expected)));
 }
 
+TEST(Dijikstra, GetPathWithSettingOfSameStartAndDestination) {
+  Dijikstra dijikstra{MotionConstraintType::CARDINAL_MOTION};
+
+  /**
+   * s = start, e = end, x = occupied
+   *  |s  | x | e |
+   *  |   | x |   |
+   *  |   |   |   |
+   */
+
+  dijikstra.SetMapStorageSize(3, 3);
+  dijikstra.SetOccupiedGrid({{1, 2}, {1, 1}});
+  dijikstra.SetStartAndDestination({0, 2}, {2, 2});
+
+  // no clearing of occupied grid
+  dijikstra.SetStartAndDestination({0, 2}, {2, 2});
+  dijikstra.FindPath();
+  EXPECT_TRUE(dijikstra.GetPath().has_value());
+
+  const auto path = dijikstra.GetPath().value();
+
+  std::vector<Coordinate> expected = {{0, 2}, {0, 1}, {0, 0}, {1, 0},
+                                      {2, 0}, {2, 1}, {2, 2}};
+  EXPECT_TRUE(std::equal(std::begin(path), std::end(path), std::begin(expected),
+                         std::end(expected)));
+
+  dijikstra.SetStartAndDestination({0, 2}, {2, 2});
+  // same start and destination, can get back previous path without finding
+  EXPECT_TRUE(dijikstra.GetPath().has_value());
+
+  dijikstra.SetStartAndDestination({0, 1}, {2, 2});
+  // different start and destination, can't get path without finding
+  EXPECT_FALSE(dijikstra.GetPath().has_value());
+
+  dijikstra.FindPath();
+  EXPECT_TRUE(dijikstra.GetPath().has_value());
+}
+
+TEST(Dijikstra, GetPathAfterReset) {
+  Dijikstra dijikstra{MotionConstraintType::CARDINAL_MOTION};
+
+  /**
+   * s = start, e = end, x = occupied
+   *  |s  | x | e |
+   *  |   | x |   |
+   *  |   |   |   |
+   */
+
+  dijikstra.SetMapStorageSize(3, 3);
+  dijikstra.SetOccupiedGrid({{1, 2}, {1, 1}});
+  dijikstra.SetStartAndDestination({0, 2}, {2, 2});
+
+  // internally clear path and occupied
+  dijikstra.Reset();
+  dijikstra.FindPath();
+  EXPECT_FALSE(dijikstra.GetPath().has_value());
+
+  dijikstra.SetStartAndDestination({0, 2}, {2, 2});
+  EXPECT_FALSE(dijikstra.GetPath().has_value());
+  dijikstra.FindPath();
+  EXPECT_TRUE(dijikstra.GetPath().has_value());
+  const auto path = dijikstra.GetPath().value();
+
+  // found path without occupied_grid
+  std::vector<Coordinate> expected = {{0, 2}, {1, 2}, {2, 2}};
+  EXPECT_TRUE(std::equal(std::begin(path), std::end(path), std::begin(expected),
+                         std::end(expected)));
+}
+
 TEST(Dijikstra, GetPathWithRevisit) {
   Dijikstra dijikstra{MotionConstraintType::CARDINAL_MOTION};
 
